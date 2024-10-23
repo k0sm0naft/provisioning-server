f5309935322c115d26d9c09c3a661b0973a4fd3e
Artem Akymenko <akimenko.artem@gmail.com>
2024-10-23 13:18:41 +0300

    Implemented ProvisioningController

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -1,11 +1,30 @@
 package com.voxloud.provisioning.controller;
 
+import com.voxloud.provisioning.service.ProvisioningService;
+import javax.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/provisioning")
+@RequiredArgsConstructor
 public class ProvisioningController {
+    private final ProvisioningService provisioningService;
 
-    // TODO Implement controller method
+    @GetMapping("/{macAddress}")
+    @ResponseStatus(HttpStatus.OK)
+    public String getProvisioning(
+            @PathVariable
+            @Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$|^([0-9A-Fa-f]{12})$",
+                    message = "Invalid MAC address format")
+            String macAddress
+    ) {
+        return provisioningService.getProvisioningFile(macAddress);
+    }
+
 }
